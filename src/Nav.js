b3063138b9afe16959d70ccedb64bee8ebe56b36
OanhBui <oanhbp3105@gmail.com>
2023-04-14 15:16:43 -0700

    apply CSS rule for Nav, create grid layout for navbar

--- src/Nav.js
+++ src/Nav.js
@@ -3,15 +3,17 @@ import Logo from './icons_assets/Logo.svg';
 function Nav() {
     return (
         <nav>
-            <img src={Logo} alt="Logo of restaurant" />
-            <ul>
-                <li><a href="/">home</a></li>
-                <li><a href="/">about</a></li>
-                <li><a href="/">menu</a></li>
-                <li><a href="/">reservations</a></li>
-                <li><a href="/">order online</a></li>
-                <li><a href="/">login</a></li>
-            </ul>
+            <div className='container'>
+                <img className='logo' src={Logo} alt="Logo of restaurant" />
+                <ul className='navbar'>
+                    <li className='home'><a href="/">home</a></li>
+                    <li className='about'><a href="/">about</a></li>
+                    <li className='menu'><a href="/">menu</a></li>
+                    <li className='reservations'><a href="/">reservations</a></li>
+                    <li className='order-online'><a href="/">order online</a></li>
+                    <li className='login'><a href="/">login</a></li>
+                </ul>
+            </div>
         </nav>
     )
 };
