2d2a8a00b4ff0642fdc40ea86d2b80a51fcfa9e8
OanhBui <oanhbp3105@gmail.com>
2023-04-20 19:08:38 -0700

    install chakra ui, update navbar hamburger icon dropdown using chakra ui

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Nav from './Nav';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <Nav />
       <Header />
       <Main />
       <Footer />
-    </>
+    </ChakraProvider>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,75 @@
 import Logo from './icons_assets/Logo.svg';
+import {
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    Spacer
+} from '@chakra-ui/react';
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+
+const Links = ['Home', 'About', 'Menu', 'Reservations', 'Order Online', 'Login'];
+
+const NavLink = ({ children }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+    </Link>
+);
 
 function Nav() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <nav>
-            <div className='container'>
-                <img className='logo' src={Logo} alt="Logo of restaurant" />
-                <ul className='navbar'>
-                    <li className='home'><a href="/">home</a></li>
-                    <li className='about'><a href="/">about</a></li>
-                    <li className='menu'><a href="/">menu</a></li>
-                    <li className='reservations'><a href="/">reservations</a></li>
-                    <li className='order-online'><a href="/">order online</a></li>
-                    <li className='login'><a href="/">login</a></li>
-                </ul>
-            </div>
-        </nav>
+        <Box className="container">
+            <Flex h={16} alignItems={'center'} w="full" justifyContent={'space-between'}>
+                    <Box marginRight={20}>
+                        <img src={Logo} />
+                    </Box>
+                    <Spacer />
+                    <Flex
+                        w="full"
+                        as={'nav'}
+                        spacing={4}
+                        display={{ base: 'none', md: 'flex' }}>
+                        {Links.map((link) => (
+                            <>
+                                <NavLink className='nav-link' key={link}>{link}</NavLink>
+                                <Spacer />
+                            </>
+                        ))}
+                    </Flex>
+
+                <IconButton
+                    size={'md'}
+                    icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                    aria-label={'Open Menu'}
+                    display={{ md: 'none' }}
+                    onClick={isOpen ? onClose : onOpen}
+                />
+            </Flex>
+
+            {isOpen ? (
+                <Box pb={4} display={{ md: 'none' }}>
+                    <Stack as={'nav'} spacing={4}>
+                        {Links.map((link) => (
+                            <NavLink key={link}>{link}</NavLink>
+                        ))}
+                    </Stack>
+                </Box>
+            ) : null}
+        </Box>
     )
 };
 
