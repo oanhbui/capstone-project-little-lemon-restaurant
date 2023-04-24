424da0d7d9ad2f01692ea651b88260d97a7c8c2e
OanhBui <oanhbp3105@gmail.com>
2023-04-24 14:29:08 -0700

    main part - menu cards

--- src/Header.js
+++ src/Header.js
@@ -14,9 +14,9 @@ function Header() {
             <Box className="container">
                 <Flex>
                     <Box w="300px">
-                        <Text fontSize="40pt" color="#F4CE14">Little Lemon</Text>
-                        <Text fontSize="20pt" color="white">Chicago</Text>
-                        <Text color="white">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</Text>
+                        <Text fontSize="40pt" color="#F4CE14" lineHeight="1">Little Lemon</Text>
+                        <Text fontSize="20pt" color="white" lineHeight="1">Chicago</Text>
+                        <Text color="white" paddingTop="10px" paddingBottom="10px">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</Text>
                         <Button bg="#F4CE14" borderRadius="4mm">Reserve a Table</Button>
                     </Box>
                     <Spacer />
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,101 @@
+import {
+    Box,
+    Flex,
+    Spacer,
+    Text,
+    Button,
+    Image,
+    Card,
+    CardHeader,
+    Heading,
+    Stack,
+    CardBody,
+    CardFooter,
+    Avatar,
+    SimpleGrid,
+    Link,
+    
+} from '@chakra-ui/react';
+import greekSalad from "./icons_assets/greekSalad.jpg";
+import lemonDesert from "./icons_assets/lemonDessert.jpg";
+import bruchetta from "./icons_assets/bruchetta.jpg";
+
 function Main() {
     return (
         <main>
-
+            <Box className="container" marginTop="120px">
+                <Flex mb="5">
+                    <Box>
+                        <Text fontWeight="bold" fontSize="30">This weeks specials!</Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <Button bg="#F4CE14" borderRadius="4mm">Online Menu</Button>
+                    </Box>
+                </Flex>
+                <SimpleGrid columns={{base: 1, md: 3}} spacing="5">
+                    <Card borderRadius="2mm">
+                        <Image src={greekSalad} width="100%" maxHeight="130px" />
+                        <CardBody bg="#EDEFEE">
+                            <Stack>
+                                <Flex>
+                                    <Box>
+                                        <Text fontWeight="bold">Greek Salad</Text>
+                                    </Box>
+                                    <Spacer />
+                                    <Box>
+                                        <Text fontWeight="bold" color="#EE9972">$12.99</Text>
+                                    </Box>
+                                </Flex>
+                                <Text fontSize="12px">
+                                    The famous greek salad of crispy lettuce, pepper, olives, and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.
+                                </Text>
+                                <Link fontSize="12px" fontWeight="bold">Order a delivery</Link>
+                            </Stack>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <Image src={bruchetta} width="100%" maxHeight="130px"/>
+                        <CardBody bg="#EDEFEE">
+                            <Stack>
+                                <Flex>
+                                    <Box>
+                                        <Text fontWeight="bold">Bruchetta</Text>
+                                    </Box>
+                                    <Spacer />
+                                    <Box>
+                                        <Text fontWeight="bold" color="#EE9972">$5.99</Text>
+                                    </Box>
+                                </Flex>
+                                <Text fontSize="12px">
+                                    Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.
+                                </Text>
+                                <Link fontSize="12px" fontWeight="bold">Order a delivery</Link>
+                            </Stack>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <Image src={lemonDesert} width="100%" maxHeight="130px"/>
+                        <CardBody bg="#EDEFEE">
+                            <Stack>
+                                <Flex>
+                                    <Box>
+                                        <Text fontWeight="bold">Lemon Dessert</Text>
+                                    </Box>
+                                    <Spacer />
+                                    <Box>
+                                        <Text fontWeight="bold" color="#EE9972">$5.00</Text>
+                                    </Box>
+                                </Flex>
+                                <Text fontSize="12px">
+                                    This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
+                                </Text>
+                                <Link fontSize="12px" fontWeight="bold">Order a delivery</Link>
+                            </Stack>
+                        </CardBody>
+                    </Card>
+                </SimpleGrid>
+            </Box>
         </main>
     )
 };
