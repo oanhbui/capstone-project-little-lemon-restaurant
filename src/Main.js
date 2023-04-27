db5b183f28a69584b539b2d69c42e3e47da0aef1
OanhBui <oanhbp3105@gmail.com>
2023-04-27 12:31:49 -0700

    Create testimonials and about content block

--- src/Main.js
+++ src/Main.js
@@ -6,12 +6,8 @@ import {
     Button,
     Image,
     Card,
-    CardHeader,
-    Heading,
     Stack,
     CardBody,
-    CardFooter,
-    Avatar,
     SimpleGrid,
     Link,
     
@@ -19,6 +15,8 @@ import {
 import greekSalad from "./icons_assets/greekSalad.jpg";
 import lemonDesert from "./icons_assets/lemonDessert.jpg";
 import bruchetta from "./icons_assets/bruchetta.jpg";
+import adriAndMariA from "./icons_assets/Mario and Adrian A.jpg";
+import adriAndMariB from "./icons_assets/Mario and Adrian b.jpg";
 
 function Main() {
     return (
@@ -94,6 +92,87 @@ function Main() {
                             </Stack>
                         </CardBody>
                     </Card>
+                </SimpleGrid>
+            </Box>
+
+            <Box className='testimonials' mt="50px" bg="#495e57">
+                <Stack className='container' py="60px">
+                    <Text textAlign="center" color="white" fontWeight="bold" fontSize="25px">Testimonials</Text>
+                    <SimpleGrid columns={{base: 1, md: 4}} spacing="4">
+                        <Box bg="white">
+                            <Stack px="10px" py="10px">
+                                <Text>Rating</Text>
+                                <SimpleGrid columns={{base: 1, md: 2}} spacing="3">
+                                    <Box>
+                                        <Image src={greekSalad} />
+                                    </Box>
+                                    <Box>
+                                        <Text>Name</Text>
+                                    </Box>
+                                </SimpleGrid>
+                                <Text>Review Text</Text>
+                            </Stack>
+                        </Box>
+                        <Box bg="white">
+                            <Stack px="10px" py="10px">
+                                <Text>Rating</Text>
+                                <SimpleGrid columns={{base: 1, md: 2}} spacing="3">
+                                    <Box>
+                                        <Image src={greekSalad} />
+                                    </Box>
+                                    <Box>
+                                        <Text>Name</Text>
+                                    </Box>
+                                </SimpleGrid>
+                                <Text>Review Text</Text>
+                            </Stack>
+                        </Box>
+                        <Box bg="white">
+                            <Stack px="10px" py="10px">
+                                <Text>Rating</Text>
+                                <SimpleGrid columns={{base: 1, md: 2}} spacing="3">
+                                    <Box>
+                                        <Image src={greekSalad} />
+                                    </Box>
+                                    <Box>
+                                        <Text>Name</Text>
+                                    </Box>
+                                </SimpleGrid>
+                                <Text>Review Text</Text>
+                            </Stack>
+                        </Box>
+                        <Box bg="white">
+                            <Stack px="10px" py="10px">
+                                <Text>Rating</Text>
+                                <SimpleGrid columns={{base: 1, md: 2}} spacing="3">
+                                    <Box>
+                                        <Image src={greekSalad} />
+                                    </Box>
+                                    <Box>
+                                        <Text>Name</Text>
+                                    </Box>
+                                </SimpleGrid>
+                                <Text>Review Text</Text>
+                            </Stack>
+                        </Box>
+                    </SimpleGrid>
+                </Stack>
+            </Box>
+
+            <Box className='about container' mt="50px" mb="50">
+                <SimpleGrid columns={{base: 1, md: 2}} spacing="5">
+                    <Box>
+                        <Text>Little Lemon</Text>
+                        <Text>Chicago</Text>
+                        <Text>
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+                        </Text>
+                    </Box>
+                    <Box position="relative" height="150%" width="75%">
+                        <Image src={adriAndMariA} position="absolute" left="0" bottom="0" width="60%" zIndex="2"/>
+                        <Image src={adriAndMariB} position="absolute" top="0" right="0" width="60%" zIndex="1" />
+                    </Box>
                 </SimpleGrid>
             </Box>
         </main>
